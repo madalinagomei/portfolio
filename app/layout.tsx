a5681ef0506338fb4a928390cfffb5b28518d343
Madalina Gomei <madalinagomei@gmail.com>
2024-09-09 13:45:45 +0200

    add header, change color bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={"${inter.className} bg-gray-50 text-gray-950 relative"}>
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-[#f6b8e5] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#c1eafd] absolute top-[-1rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         {children}
       </body>
     </html>
   );
 }
-// min 44>45
+// min 50-55
