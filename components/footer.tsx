3f7365ee4e20983c60cc60ef5fc6ac760e4bfa0c
Madalina Gomei <madalinagomei@gmail.com>
2024-09-15 22:27:58 +0200

    dark mode and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">
+        &copy; 2024 Mădălina Gomei. All rights reserved.
+      </small>
+      <p className="text-xs">
+        <span className="font-semibold">About this website:</span> built with
+        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
+        Framer Motion, React Email & Resend, Vercel hosting.
+      </p>
+    </footer>
+  );
 }
