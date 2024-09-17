598333233d8e4aef7456ed804bbe2c4c1b8638c5
Madalina Gomei <madalinagomei@gmail.com>
2024-09-17 13:46:04 +0200

    add footer, change theme etc

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
       <Skills />
       <Experience />
       <Contact />
-      <Footer />
     </main>
   );
 }
--- components/contact.tsx
+++ components/contact.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import React, { experimental_useEffectEvent } from "react";
+import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@/actions/sendEmail";
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
 import SubmitBtn from "./submit-btn";
 import toast from "react-hot-toast";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
-  const { pending } = useFormStatus();
 
   return (
     <motion.section
