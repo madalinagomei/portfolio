3a12c45bae9a1618c3ad338a605191cb2081cfe8
Madalina Gomei <madalinagomei@gmail.com>
2025-02-05 16:22:03 +0100

    try to make live on git

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //   images: {
-  //     remotePatterns: [
-  //       {
-  //         protocol: "https",
-  //         hostname: "images.unsplash.com",
-  //       },
-  //     ],
-  //   },
-  // experimental: {
-  //   serverActions: true,
-  // },
+  output: "export",
+  experimental: {
+    serverActions: true,
+  },
 };
 module.exports = nextConfig;
